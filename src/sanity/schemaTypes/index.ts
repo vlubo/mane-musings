27ba4c17593be6104cefcfd61924f3b8335623c1
vlubo <tempurajason@gmail.com>
2026-04-21 20:12:05 +1200

    removed schema

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -3,9 +3,7 @@ import { category } from './category'
 import { hairType } from './hairType'
 import { affiliateLink } from './affiliateLink'
 import { post } from './post'
-import {postType} from './postType'
 
-export const schemaTypes = [postType]
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [category, hairType, affiliateLink, post],
 }
